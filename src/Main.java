b7a7ffaba8ec3b3da2f890144beff5b8447ec166
Sergey Boltyshov <boltyshovs@yandex.ru>
2023-04-05 15:28:40 +0300

    add predicat

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
 
-        Comparator<Person> comp = new ComparatorNamePerson();
-        //TreeSet<Person> personList = new TreeSet<>(comp);
         List<Person> personList = new ArrayList<>();
+        Predicate<Person> personPredicate = a -> a.getAge() < 18;
 
         personList.add(new Person("Petya", "Pet", 25));
-        personList.add(new Person("Vanya", "Peta", 18));
-        personList.add(new Person("Olya", "Petrova", 22));
+        personList.add(new Person("Vanya", "Peta", 17));
+        personList.add(new Person("Olya", "Petrova", 15));
         personList.add(new Person("Sergey", "Sergeev", 35));
+        personList.add(new Person("Pashsa", "Pashov", 40));
 
-        Collections.sort(personList, comp);
+        personList.removeIf(personPredicate);
 
         System.out.println(personList);
 
